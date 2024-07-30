6f5d687de5e88a20d94e3f74017d15a0ed180cd7
mobin <mobinizadi1998@gmail.com>
2024-07-30 21:09:05 +0330

    show massage in console log

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-function isLogin(username){
-   if(username=='maryam'){
-    return true
-   }
-=======
 function isLogin(name){
     if(name ==='mobin'){
         return true
     }
->>>>>>> fixBugIsLogin
 }
+
+console.log('test program');
