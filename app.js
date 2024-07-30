706bc3336165a9032ad3e7e05d6639a1fbe904db
mobin <mobinizadi1998@gmail.com>
2024-07-30 19:09:23 +0330

    create function login user

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function isLogin(user){
     return true
 }
+
+function login(user){
+    if(user.name==='mobin' && user.password===123456){
+        return true
+    }else{
+        return false
+    }
+}
